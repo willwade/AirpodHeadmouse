5f1584a49a1e0ed80ea1d1162c8c071977782686
will wade <willwade@gmail.com>
2025-10-05 22:04:54 +0100

    Initial Commit

---
+++ AirpodHeadMouse/AirpodHeadMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirpodHeadMouseApp.swift
+//  AirpodHeadMouse
+//
+//  Created by AX North on 05/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AirpodHeadMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
